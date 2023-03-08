07a6941a6a662e058ce505689f401cd89ab9ee22
tmaasen <tmaasen@gmail.com>
2023-03-07 22:17:33 -0600

    Added DI System using Caliburn

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,40 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper: BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            // Only use singleton classes when you have to. One ongoing instance is less efficient for memory than creating an instance on demand, using it, and then removing it
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
 
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModel => _container.RegisterPerRequest(viewModel, viewModel.ToString(), viewModel));
+        }
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
